5b829e82615674545dbd1db57733f2e310da529c
aufung1999 <78595669+aufung1999@users.noreply.github.com>
2023-07-14 23:07:52 -0400

    cont.

--- app/User/Components/Client.tsx
+++ app/User/Components/Client.tsx
@@ -32,51 +32,35 @@ export default function UserClient({ resumeData }: { resumeData: any }) {
       className=" mt-5 grid grid-cols-4 gap-3 xl:grid-cols-3 place-items-start relative "
     >
       {resume_csr?.map((each, i: number) => (
-        <div key={i} className="group  ">
-          <div className=" group-hover:h-screen hover:border-8 ">
-            <div
-              className="group-hover:scale-[2.0] transition duration-500 border-4 "
-              onClick={() => ClickHandler(each._id)}
-            >
-              <div className="flex-col ">
-                <div className="border border-green-500 bottom-0 px-2 bg-white">
-                  <div className="flex">
-                    <b className=" w-1/4 flex group-hover:justify-end">
-                      Position
-                    </b>
-                    <b>:</b>
-                    <div className=" w-3/4">
-                      {each.job_details.job_position}
-                    </div>
-                  </div>
-                  <div className="flex">
-                    <b className=" w-1/4 flex group-hover:justify-end">
-                      Company
-                    </b>
-                    <b>:</b>
-                    <div className="  w-3/4">
-                      {each.job_details.company_name}
-                    </div>
-                  </div>
-                  <div className="flex">
-                    <b className=" w-1/4 flex group-hover:justify-end">
-                      Website
-                    </b>
-                    <b>:</b>
-                    <div className=" w-3/4 ">{each.job_details.website}</div>
-                  </div>
-                  <div className="flex">
-                    <b className=" w-1/4 flex group-hover:justify-end">Date</b>
-                    <b>:</b>
-                    <div className=" w-3/4 ">
-                      {each.createdAt.substring(0, 10)}
-                    </div>
-                  </div>
-                </div>
-                <div className=" border border-red-500">
-                  <img src={each.image.src} />
-                </div>
-              </div>
+        <div key={i} className="group ">
+          <div className="border border-green-500 bottom-0 px-2 bg-white">
+            <div className="flex">
+              <b className=" w-1/4 flex group-hover:justify-end">Position</b>
+              <b>:</b>
+              <div className=" w-3/4">{each.job_details.job_position}</div>
+            </div>
+            <div className="flex">
+              <b className=" w-1/4 flex group-hover:justify-end">Company</b>
+              <b>:</b>
+              <div className="  w-3/4">{each.job_details.company_name}</div>
+            </div>
+            <div className="flex">
+              <b className=" w-1/4 flex group-hover:justify-end">Website</b>
+              <b>:</b>
+              <div className=" w-3/4 ">{each.job_details.website}</div>
+            </div>
+            <div className="flex">
+              <b className=" w-1/4 flex group-hover:justify-end">Date</b>
+              <b>:</b>
+              <div className=" w-3/4 ">{each.createdAt.substring(0, 10)}</div>
+            </div>
+          </div>
+          <div
+            className="group-hover:scale-[1.7] transition duration-500 border-4 "
+            onClick={() => ClickHandler(each._id)}
+          >
+            <div className=" border border-red-500">
+              <img src={each.image.src} />
             </div>
           </div>
         </div>
