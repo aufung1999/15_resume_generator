d7e351b79081aabf04939848145e782f8d6e86d0
aufung1999 <78595669+aufung1999@users.noreply.github.com>
2023-08-27 18:56:53 -0400

    add 'Promise.all' to Education API

--- app/api/user/education/route.ts
+++ app/api/user/education/route.ts
@@ -38,63 +38,65 @@ export async function POST(req: NextRequest, res: NextApiResponse) {
   if (session) {
     const body = await req.json();
 
-    await db.connect();
-    body.map(async (each: EducationState) => {
-      const {
-        index,
-        SchoolName,
-        Degree,
-        Subject,
-        current,
-        StartDate,
-        EndDate,
-      } = each;
+    await Promise.all(
+      body.map(async (each: EducationState) => {
+        await db.connect();
+        const {
+          index,
+          SchoolName,
+          Degree,
+          Subject,
+          current,
+          StartDate,
+          EndDate,
+        } = each;
 
-      //use the email from "Next-auth" to find the data in "Education" collection
-      const exist = await Education.findOne({
-        email: session?.user?.email,
-        index: index,
-      });
-      //***/
-
-      //if "Education" collction has the data
-      if (exist) {
-        const filter = { email: session?.user?.email };
-        const update = {
-          index: index,
-          SchoolName: SchoolName,
-          Degree: Degree,
-          Subject: Subject,
-          current: current,
-          StartDate: StartDate,
-          EndDate: EndDate,
-        };
-
-        // `doc` is the document _after_ `update` was applied because of
-        // `new: true`
-        await Education.findOneAndUpdate(filter, update, {
-          new: true,
-        });
-      }
-      //***/
-
-      //if "Education" collction does NOT have the data
-      if (!exist) {
-        const work = await new Education({
+        //use the email from "Next-auth" to find the data in "Education" collection
+        const exist = await Education.findOne({
           email: session?.user?.email,
           index: index,
-          SchoolName: SchoolName,
-          Degree: Degree,
-          Subject: Subject,
-          current: current,
-          StartDate: StartDate,
-          EndDate: EndDate,
         });
+        //***/
 
-        await work.save();
-      }
-      //***/
-    });
+        //if "Education" collction has the data
+        if (exist) {
+          const filter = { email: session?.user?.email };
+          const update = {
+            index: index,
+            SchoolName: SchoolName,
+            Degree: Degree,
+            Subject: Subject,
+            current: current,
+            StartDate: StartDate,
+            EndDate: EndDate,
+          };
+
+          // `doc` is the document _after_ `update` was applied because of
+          // `new: true`
+          await Education.findOneAndUpdate(filter, update, {
+            new: true,
+          });
+        }
+        //***/
+
+        //if "Education" collction does NOT have the data
+        if (!exist) {
+          const work = await new Education({
+            email: session?.user?.email,
+            index: index,
+            SchoolName: SchoolName,
+            Degree: Degree,
+            Subject: Subject,
+            current: current,
+            StartDate: StartDate,
+            EndDate: EndDate,
+          });
+
+          await work.save();
+        }
+        //***/
+      })
+    );
     await db.disconnect();
 
     return NextResponse.json({ message: "Hello" });
