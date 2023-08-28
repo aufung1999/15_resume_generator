54bddf024a97fe3663dd7682f08f0757f1337792
aufung1999 <78595669+aufung1999@users.noreply.github.com>
2023-08-28 18:49:57 -0400

    cont.

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,16 +2,15 @@
 // import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 // import ClientComponent from "@/components/ClientComponent";
 
-// export default async function Home() {
-//   const session = await getServerSession(authOptions);
-
-//   return (
-//     <div className=" flex flex-col justify-center items-center h-screen bg-white w-screen overflow-hidden">
-//       <h1>Home</h1>
-//       <h1>Server Side Rendered</h1>
-//       <pre>{JSON.stringify(session)}</pre>
-//       <h1>Client Side Rendered</h1>
-//       <ClientComponent/>
-//     </div>
-//   );
-// }
+export default async function Home() {
+  //   const session = await getServerSession(authOptions);
+  //   return (
+  //     <div className=" flex flex-col justify-center items-center h-screen bg-white w-screen overflow-hidden">
+  //       <h1>Home</h1>
+  //       <h1>Server Side Rendered</h1>
+  //       <pre>{JSON.stringify(session)}</pre>
+  //       <h1>Client Side Rendered</h1>
+  //       <ClientComponent/>
+  //     </div>
+  //   );
+}
