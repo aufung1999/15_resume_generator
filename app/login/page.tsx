62016c2c7515ac37f5628325da54556cd802f875
aufung1999 <78595669+aufung1999@users.noreply.github.com>
2023-09-07 15:04:20 -0400

    demo account

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -72,6 +72,7 @@ export default function Login() {
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   onChange={(e) => setData({ ...data, email: e.target.value })}
                 />
+                <div className=" text-gray-500">abc123@gmail.com</div>
               </div>
             </div>
 
@@ -104,6 +105,7 @@ export default function Login() {
                     setData({ ...data, password: e.target.value })
                   }
                 />
+                <div className=" text-gray-500">123456</div>
               </div>
             </div>
 
